ad7ff5c31140d2aac07922398980298a179d7fdc
SeulKi <72595355+seulki9802@users.noreply.github.com>
2021-02-16 16:43:02 +0900

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,10 @@
+#!C:\Users\chosun\AppData\Local\Programs\Python\Python37-32\python.exe
+print("content-type: text/html; charset=utf-8\n")
+
+a = 3+4+5
+b = a + 3
+
+print('''
+<h1> <a href="http://cslkr.iptime.org">Hello</a></h1>
+<h1> {0} </h1>
+'''.format(b))
